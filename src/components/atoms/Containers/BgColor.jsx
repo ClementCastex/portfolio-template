3aea282634517faa87a310d67f547287283fa04f
ClementCastex <150129015+ClementCastex@users.noreply.github.com>
2024-10-03 17:07:18 +0200

    Mon background perso

--- src/components/atoms/Containers/BgColor.jsx
+++ src/components/atoms/Containers/BgColor.jsx
@@ -2,8 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledBgColor = styled.div`
-  background-color: ${({ color, theme }) =>
-    color || theme.colors.backgroundLight};
+  background: ${({ color, theme }) =>
+    color ||
+    `linear-gradient(-45deg, #402162, ${theme.colors.backgroundLight}, #402162)`};
+  background-size: 400% 400%;
+  animation: gradient 8s ease infinite;
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 `;
 
 const BgColor = ({ color, children }) => {
